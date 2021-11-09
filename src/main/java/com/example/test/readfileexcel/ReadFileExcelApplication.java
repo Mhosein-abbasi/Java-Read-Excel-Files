4c66ce707fab571bd65f582f09b28591c0402cc7
mohammad hosein abbasi <abbasimhoseinit@gmail.com>
2021-11-09 07:50:42 -0800

    First commit

---
+++ src/main/java/com/example/test/readfileexcel/ReadFileExcelApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.readfileexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadFileExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadFileExcelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test/readfileexcel/ReadFileExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test.readfileexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadFileExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
